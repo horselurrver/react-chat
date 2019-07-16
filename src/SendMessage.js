ce0df06c87f9c68792bd13a34b4d7ca256964e16
Amy Wang <horselurrver@gmail.com>
2019-07-16 15:55:41 -0400

    Add text area

--- src/SendMessage.js
+++ src/SendMessage.js
@@ -4,7 +4,8 @@ class SendMessage extends Component {
   render() {
     return (
       <div className="send">
-        <h3 className="type-message">Type message & hit ENTER</h3>
+        <textarea>
+        </textarea>
       </div>
     );
   }

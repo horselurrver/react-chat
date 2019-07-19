80712d1027c6d984f6b51bc7a78ac13ed460ac92
Amy Wang <horselurrver@gmail.com>
2019-07-19 12:39:40 -0400

    Updates

---
+++ precache-manifest.6d30a1794c1be1ef09c55173a7f173d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57b79f19c4874dfc9cb61500f6df14f",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "387b16dc872f660efc78",
+    "url": "/react-chat/static/css/main.4ef07298.chunk.css"
+  },
+  {
+    "revision": "6d7ab2fd19f1b4b3bca8",
+    "url": "/react-chat/static/js/2.5a1be9c2.chunk.js"
+  },
+  {
+    "revision": "387b16dc872f660efc78",
+    "url": "/react-chat/static/js/main.ffc83aad.chunk.js"
+  },
+  {
+    "revision": "49fde9a2889aba467256",
+    "url": "/react-chat/static/js/runtime~main.14e77f4a.js"
+  }
+]);
